e995005149303372ebc77f2837b7fc9e26853d16
josemthiesen <josemthiesen@users.noreply.github.com>
2022-09-26 14:47:08 -0300

    Exclude dummy files

--- VisualStudio_Intel/VisualStudio_Intel/x64/Debug/help__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Mon Sep 26 14:42:23 2022
-        ! This source file is for reference only and may not completely
-        ! represent the generated interface used by the compiler.
-        MODULE HELP__genmod
-          INTERFACE 
-            SUBROUTINE HELP
-            END SUBROUTINE HELP
-          END INTERFACE 
-        END MODULE HELP__genmod
