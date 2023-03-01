bd2318c726c483375cc8e7652d95402170dccefe
josemthiesen <josemthiesen@users.noreply.github.com>
2023-03-01 17:12:02 -0300

    Correction in Kpu matrix
    Correction in the Kpu4 matrix. Turn into positive sign according to the linearization.

--- VisualStudio_Intel/VisualStudio_Intel/x64/Debug/help__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Mon Jan 16 14:06:59 2023
-        ! This source file is for reference only and may not completely
-        ! represent the generated interface used by the compiler.
-        MODULE HELP__genmod
-          INTERFACE 
-            SUBROUTINE HELP
-            END SUBROUTINE HELP
-          END INTERFACE 
-        END MODULE HELP__genmod
